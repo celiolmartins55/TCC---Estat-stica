3bd96e1d700e3ac82feaa84e10bb99b4e22e08f4
Célio João <celiojoaolucas@gmail.com>
2026-09-23 20:55:05 -0300

    Add files via upload

---
+++ 1análise descritiva.R
@@ -0,0 +1,20 @@
+#Análise descritiva 
+dados <- read.csv("C:\\Users\\Celinho\\Downloads\\dados_tcc\\seriea0324.csv")
+partic <- read.csv("C:\\Users\\Celinho\\Downloads\\classificacao_brasileirao_todos_os_times_2003_2024 (1).csv")
+dist <-  read.csv("C:\\Users\\Celinho\\Downloads\\dados_tcc\\distanciapercorrida.csv")
+descri <- function(x) {
+  return(c(
+    Min = min(x, na.rm = TRUE),
+    Max = max(x, na.rm = TRUE),
+    Media  = mean(x, na.rm = TRUE),
+    var  = var(x, na.rm = TRUE),
+    cv  = sd(x, na.rm = TRUE)/mean(x,na.rm = TRUE)*100
+  ))
+}
+attach(dados)
+descri(gols_mandante);descri(gols_visitante)
+descri(partic$Total_Participacoes)
+descri(publico[publico!=0])
+valor <- subset(dados, ano_campeonato >= 2008)
+descri(valor$valor_equipe_titular_mandante);descri(valor$valor_equipe_titular_visitante)
+descri(dist$Total_Viagens_KM)
